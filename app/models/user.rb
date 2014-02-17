ef74577c4298a0d23a70adba2f12ec0cd123776d
Thuong Ho <thuong.t.ho@gmail.com>
2014-02-17 15:38:00 -0800

    added homepage with styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
   has_many :microposts
+
+  validates_presence_of :name, :email
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 DemoApp::Application.routes.draw do
+  root 'users#index'
+
   resources :microposts
 
   resources :users
 
+  match ':controller(/:action(/:id))', :via => [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
