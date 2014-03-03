151cc0a3ce29f959a25b9c9041c7353e75ef07db
Thuong Ho <thuong.t.ho@gmail.com>
2014-03-03 15:00:57 -0800

    finish contact page with tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,4 +42,52 @@ describe "Static pages" do
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
+
+  describe "Contact page" do
+    
+    it "should have content 'CS 232 Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+
+    it "should have an h1 element with class name 'page-title'" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('h1.page-title')
+    end
+
+    it "should have a Definition List <dl> element" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('dl')
+    end
+
+    it "should have a Definition Term <dt> element" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('dt')
+    end
+
+    it "should have a Definition Description <dd> element" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('dd')
+    end
+
+    it "should have a section with class name 'main'" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('section.main')
+    end
+
+    it "should have an h1.page-title selector in the CSS" do
+      visit '/static_pages/contact'
+      expect(page).to have_css('h1.page-title')
+    end
+
+    it "should have a .main selector in the CSS" do
+      visit '/static_pages/contact'
+      expect(page).to have_css('.main')
+    end
+  end
 end
