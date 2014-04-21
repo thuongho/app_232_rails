b937ae2e8703b0b3b435cf0b214ff02e33e27df1
Thuong Ho <thuong.t.ho@gmail.com>
2014-04-21 16:02:33 -0700

    refactor fill_in_form method

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,12 +31,13 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      # before do
+      #   fill_in "Name",         with: "Example User"
+      #   fill_in "Email",        with: "user@example.com"
+      #   fill_in "Password",     with: "foobar"
+      #   fill_in "Confirmation", with: "foobar"
+      # end
+      before { fill_in_form }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,10 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+def fill_in_form
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
